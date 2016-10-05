236bd5075ecb2727a89670f075db268764af654e
FabianEscobar <fabian.escobar@usach.cl>
2016-10-05 11:01:46 -0300

    Arreglo al usar argv
    Se cambió argv[0] por argv[1] para leer el nombre del archivo
    correctamente.

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
-//#include <pthread.h>
+#include <pthread.h>
 #include "readFile.h"
 #include "funciones.h"
 
 int main(int argc, char *argv[]){
-    FILE *archivo=fopen(argv[0],"r");
+    FILE *archivo=fopen(argv[1],"r");
     int *parametros=fgetParameters(archivo);
     int ancho=parametros[0];
     int largo=parametros[1];
