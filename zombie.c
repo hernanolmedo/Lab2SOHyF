ba7b43a63566accbadfe87e97cfd201744df7845
hernanolmedo <hernan.olmedo@usach.cl>
2016-10-14 13:01:21 -0300

    Cambios zombie.c
    Todavía quedan algunos errores, pero al parecer son de linkeo y solucionables con makefile.

--- zombie.c
+++ zombie.c
@@ -1,9 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
 #include "zombie.h"
 
 void* zombieFunc (void* insZombie){ // Puede ser que se decida usar "no_usado" después. Cambiar el nombre en caso de que así sea.
     zombie* zombiePointer=(zombie*)insZombie;
-    while(){
-
+    while(1){
         pthread_barrier_wait (&barrera);
     }
     // En este punto ya todas las hebras habrán terminado de hacer lo que tengan que hacer durante el turno.
