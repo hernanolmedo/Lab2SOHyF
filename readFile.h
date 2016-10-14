11fe7b9a5cb4e0d9331c178920b7ed7b6f3207fb
hernanolmedo <hernan.olmedo@usach.cl>
2016-10-14 13:00:41 -0300

    Cambios readFile.h
    Todavía quedan algunos errores, pero al parecer son de linkeo y solucionables con makefile.

--- readFile.h
+++ readFile.h
@@ -1,7 +1,10 @@
+#include "zombie.h"
+#include "person.h"
+
 #ifndef READFILE_H_INCLUDED
 #define READFILE_H_INCLUDED
 
 int* fgetParameters (FILE* filePointer);
-char** fgetMatrix(FILE* file,int height,int width,void *peopleArray,void *zombieArray,int ammo);
+char** fgetMatrix(FILE* file,int height,int width,person *peopleArray,zombie *zombieArray,int ammo);
 
 #endif // READFILE_H_INCLUDED
