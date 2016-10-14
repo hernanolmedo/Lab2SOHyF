ef9476153cc4be9fbb5f665c79d6586768e6d10e
hernanolmedo <hernan.olmedo@usach.cl>
2016-10-14 12:57:41 -0300

    Cambios function.h
    Todavía quedan algunos errores, pero al parecer son de linkeo y solucionables con makefile.

--- functions.h
+++ functions.h
@@ -1,13 +1,15 @@
 #ifndef FUNCTIONS_H_INCLUDED
 #define FUNCTIONS_H_INCLUDED
 
+extern char** matriz;
+extern int ** infoMatrix;
 int randomPosition();
 int whoLives();
 void printScreen(int height,int width,char **matrix);
 void move(int posX,int posY,char **matrix);
 int positionCheker(int posX,int posY,char **matrix);
 int gameOver(int height,int width,char **matrix);
-void shoot(int posX,int posY,char **matrix,int **infoMatrix);
+void shoot(int posX,int posY,char **matrix,int **infoMatrix,int* ammo);
 int dead(int posX,int posY);
 void corpses(int height,int width,char **matrix,int **infoMatrix);
 
