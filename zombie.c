c6415b71edab7f5c682747681176a97e8db45a4e
hernanolmedo <hernan.olmedo@usach.cl>
2016-10-19 14:47:36 -0300

    Warnings solved

--- zombie.c
+++ zombie.c
@@ -2,11 +2,14 @@
 #include <stdio.h>
 #include <pthread.h>
 #include "zombie.h"
+#include "functions.h"
 
 void* zombieFunc (void* insZombie){ // Puede ser que se decida usar "no_usado" después. Cambiar el nombre en caso de que así sea.
     zombie* zombiePointer=(zombie*)insZombie;
-    while(1){
+    while(matriz[zombiePointer->posX][zombiePointer->posY]!='z'){ // Mientras la persona esta viva
+        move(zombiePointer->posX,zombiePointer->posY,matriz);
         pthread_barrier_wait (&barrera);
     }
+	return 0;
     // En este punto ya todas las hebras habrán terminado de hacer lo que tengan que hacer durante el turno.
 }
