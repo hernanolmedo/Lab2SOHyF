46c35bf8a211c570f1aaae47b2374e65f4514a13
hernanolmedo <hernan.olmedo@usach.cl>
2016-10-19 14:47:03 -0300

    Warnings solved

--- person.c
+++ person.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <pthread.h>
 #include "person.h"
+#include "functions.h"
 
 void* personFunc (void* insPerson){ // Puede ser que se decida usar "no_usado" despu�s. Cambiar el nombre en caso de que as� sea.
     person* personPointer=(person*)insPerson;
@@ -10,5 +11,6 @@ void* personFunc (void* insPerson){ // Puede ser que se decida usar "no_usado" d
         shoot(personPointer->posX,personPointer->posY,matriz,infoMatrix,&personPointer->ammo);
         pthread_barrier_wait (&barrera);
     }
+	return 0;
     // En este punto ya todas las hebras habr�n terminado de hacer lo que ten�an que hacer durante el turno.
 }
